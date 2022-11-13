5d02a358a76d77931ba15eb3f86555f80933bafb
oxyn <98268537+OxynDev@users.noreply.github.com>
2022-11-13 21:32:45 +0100

    Create ReadMe.py

---
+++ ReadMe.py
@@ -0,0 +1,57 @@
+import httpx
+
+class Yo:
+    def __init__(self) -> None:
+        self.name = "Lucas" # My name sound like this 
+        self.Age = 17
+
+    def Dm(self) -> str:
+        print("Discord -> oxyn#3520")
+        return "👋"
+    
+    def AboutMe(self):
+        print('I go to mechanical school but love programming LoL')
+
+    def Achievements(self):
+        Yep = [
+            'Func4ptcha Solver and Bypass',
+            'Hc4ptcha Solver',
+            'Tw1tch Gay 4m Accounts lol' + httpx.get("https://bit.ly/3hG1LE7"), # ScreenShot
+            'And more...'
+        ]
+
+class Dev:
+    def __init__(self) -> None:
+        pass
+    
+    def Code(self):
+        dev = {
+            'Python 👍': 'gay but still nice',
+            'Javascript 😏': 'reverse engineer',
+            'C# 💀': 'who use this shit',
+        } 
+    
+    def Web(self):
+        dev = {
+            'Php 🧐': 'I know basics',
+            'Js 👌': 'Cool',
+            'Html 🤙': 'All kids and skids know this',
+            'Css 😱': 'I love design'
+        }
+        backend = ['Php', 'Flask', 'Express']
+        return 'Fullstack'
+
+class Shop:
+    def __init__(self) -> None:
+        pass
+    def Proxy(self):
+        'uHq / Privat / Cheap'
+        {
+            "Ip": 2000,
+            "Auth": "Ip",
+            "Threads": 150,
+            "Price": "5$",
+            "Time": "Month"
+        }
+        # CHECK IT 👀
+        httpx.get("https://discord.gg/dVaDEEnF7u")
